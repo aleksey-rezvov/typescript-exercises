24cefc541224cb3bff24011a2f5203dcd3ad292a
Aleksey Rezvov <aleksey.rezvov@gmail.com>
2020-05-23 02:23:10 +0300

    Tasks with types were done.

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,12 @@
-declare module 'str-utils' {
-    // export const ...
-    // export function ...
+declare module "str-utils" {
+  // export const ...
+  // export function ...
+  interface StrUtilsFunc {
+    (value: string): string;
+  }
+  export const strReverse: StrUtilsFunc;
+  export const strToLower: StrUtilsFunc;
+  export const strToUpper: StrUtilsFunc;
+  export const strRandomize: StrUtilsFunc;
+  export const strInvertCase: StrUtilsFunc;
 }
--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,34 @@
-declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+declare module "stats" {
+  type Comparator<T> = (a: T, b: T) => number;
+  type StatIndexFunc = <T>(
+    input: Array<T>,
+    comparator: Comparator<T>
+  ) => number;
+  type StatValueFunc = <T>(
+    input: Array<T>,
+    comparator: Comparator<T>
+  ) => T | null;
+
+  //   export interface StatFunc<T> {
+  //     (input: Array<T>, comparator: Comparator<T>): number;
+  //     // (input: Array<T>, comparator: Comparator<T>): T | null;
+  //   }
+
+  export const getMaxIndex: StatIndexFunc;
+  export const getMaxElement: StatValueFunc;
+  export const getMinIndex: StatIndexFunc;
+  export const getMinElement: StatValueFunc;
+  export const getMedianIndex: StatIndexFunc;
+  export const getMedianElement: StatValueFunc;
+  export const getAverageValue: StatValueFunc;
 }
+
+/* 
+    getMaxIndex,
+    getMaxElement,
+    getMinIndex,
+    getMinElement,
+    getMedianIndex,
+    getMedianElement,
+    getAverageValue
+     */
